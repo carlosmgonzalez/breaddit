aa82810bd0996c64959da7da5ae5ab32b0452486
Carlos Gonzalez <carlosmgonzalez1998@gmail.com>
2023-09-05 23:17:18 -0300

    Arreglando informacion del subreddit en el movil

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -66,7 +66,7 @@ export default async function Layout({children, params}: Props) {
           </div>
       
           {/* info sidebar */}
-          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+          <div className="block md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4">
               <p className="font-semibold py-3">
                 About r/{name}
